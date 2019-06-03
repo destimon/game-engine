3b20d29c6d6337d6886ceee8d0ac959fa8928bde
dimas <destaom@gmail.com>
2019-06-03 18:40:25 +0300

    Timestamp + basic output

---
+++ src/Core.cpp
@@ -0,0 +1,37 @@
+//
+// Created by desta on 03.06.2019.
+//
+
+#include "Core.h"
+#include <iostream>
+#include <cstdio>
+#include <ctime>
+#include <chrono>
+#include <cstring>
+#include <stdio.h>
+#include <time.h>
+#include <windows.h>
+
+Core::Core() {}
+
+Core::~Core() {
+
+}
+
+int Core::build() {
+
+
+    std::cout << "[" << this->getCurrentTime() << "] " << "Build start..." << std::endl;
+    Sleep(3000);
+    std::cout << "[" << this->getCurrentTime() << "]" << " Build finished!" << std::endl;
+    return 0;
+}
+
+char *Core::getCurrentTime() {
+    auto start = std::chrono::system_clock::now();
+    auto end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end-start;
+    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+
+    return (strtok(std::ctime(&end_time), "\n"));
+}
---
+++ src/Core.h
@@ -0,0 +1,19 @@
+//
+// Created by desta on 03.06.2019.
+//
+
+#ifndef UNTITLED_CORE_H
+#define UNTITLED_CORE_H
+
+
+class Core {
+public:
+    Core();
+    virtual ~Core();
+
+    int build();
+    char* getCurrentTime();
+};
+
+
+#endif //UNTITLED_CORE_H
